4b96b3d3b9a53232fb3effe9bb60db9a587ea0df
Hailey Strauss <hstrauss2001@gmail.com>
2023-10-24 16:10:24 -0400

    made the api's work

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ var searchSection = document.querySelector("#search");
 initializePage();
 
 function initializePage() {
-  searchedCitiesContainer.style.display = 'none';
+  searchedCitiesContainer.style.display = "none";
   setCurrentTime();
   saveSearchedCities();
 }
@@ -22,7 +22,7 @@ function initializePage() {
 // Set the current time
 function setCurrentTime() {
   function updateTime() {
-    let currentTime = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    let currentTime = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a");
     document.getElementById("currentTime").textContent = currentTime;
   }
   updateTime();
@@ -32,15 +32,126 @@ function setCurrentTime() {
 // Search button event listener
 searchButton.addEventListener("click", function () {
   var cityToSearch = searchInput.value.trim();
-  if (cityToSearch === '') return;
+  if (cityToSearch === "") return;
   searchWeather(cityToSearch);
   saveSearchedCityButton(cityToSearch);
   saveCity(cityToSearch);
   searchInput.value = "";
-  searchSection.classList.remove('col-12')
-  searchSection.classList.add('col-4')
-  searchedCitiesContainer.style.display = '';
+  searchSection.classList.remove("col-12");
+  searchSection.classList.add("col-4");
+  searchedCitiesContainer.style.display = "";
 });
+
+function searchWeather(city) {
+  var apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("City not found");
+      }
+    })
+    .then(function (data) {
+      displayWeather(data);
+    })
+    .catch(function (error) {
+      console.error(error);
+      alert("City not found. Please enter a valid city name.");
+    });
+}
+
+function displayWeather(data) {
+  var cityName = data.name;
+  var temperature = data.main.temp;
+  var windSpeed = data.wind.speed;
+  var humidity = data.main.humidity;
+
+  cityTitle.textContent = cityName;
+  temperatureElement.textContent = temperature + "°F";
+  windElement.textContent = "Wind: " + windSpeed + " mph";
+  humidityElement.textContent = "Humidity: " + humidity + "%";
+}
+function searchWeather(city) {
+  var currentWeatherApiKey = "7122c388f26a175ce76367c47d174de3"; // Replace with your current weather API key
+  var forecastApiKey = "7122c388f26a175ce76367c47d174de3"; // Replace with your forecast API key
+
+  // API endpoint for current weather
+  var currentWeatherApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${currentWeatherApiKey}`;
+
+  // API endpoint for 5-day/3-hour forecast
+  var forecastApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${forecastApiKey}`;
+
+  // Fetch current weather data
+  fetch(currentWeatherApiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("City not found");
+      }
+    })
+    .then(function (currentWeatherData) {
+      // Fetch forecast data after successfully fetching current weather
+      fetch(forecastApiUrl)
+        .then(function (response) {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Forecast data not available");
+          }
+        })
+        .then(function (forecastData) {
+          displayWeather(currentWeatherData, forecastData);
+        })
+        .catch(function (error) {
+          console.error(error);
+          alert("Forecast data not available.");
+        });
+    })
+    .catch(function (error) {
+      console.error(error);
+      alert("City not found. Please enter a valid city name.");
+    });
+}
+
+function displayWeather(currentWeatherData, forecastData) {
+  // Display current weather information
+  var cityName = currentWeatherData.name;
+  var temperature = currentWeatherData.main.temp;
+  var windSpeed = currentWeatherData.wind.speed;
+  var humidity = currentWeatherData.main.humidity;
+
+  cityTitle.textContent = cityName;
+  temperatureElement.textContent = temperature + "°F";
+  windElement.textContent = "Wind: " + windSpeed + " mph";
+  humidityElement.textContent = "Humidity: " + humidity + "%";
+
+  // Display 5-day/3-hour forecast information (modify as needed)
+  var forecastList = forecastData.list;
+  forecastContainer.innerHTML = ""; // Clear previous forecast data
+
+  for (var i = 0; i < forecastList.length; i++) {
+    var forecastItem = forecastList[i];
+    var forecastTime = forecastItem.dt_txt;
+    var forecastTemperature = forecastItem.main.temp;
+    var forecastWindSpeed = forecastItem.wind.speed;
+    var forecastHumidity = forecastItem.main.humidity;
+
+    var forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+    forecastCard.innerHTML = `
+        <h4>${forecastTime}</h4>
+        <p>Temperature: ${forecastTemperature}°F</p>
+        <p>Wind: ${forecastWindSpeed} mph</p>
+        <p>Humidity: ${forecastHumidity}%</p>
+      `;
+
+    forecastContainer.appendChild(forecastCard);
+  }
+}
 
 // Dynamic searched cities
 function saveSearchedCityButton(cityToSearch) {
@@ -56,9 +167,9 @@ function saveSearchedCityButton(cityToSearch) {
 
 function searchCity(cityToSearch) {
   searchWeather(cityToSearch);
-  searchSection.classList.remove('col-12')
-  searchSection.classList.add('col-4')
-  searchedCitiesContainer.style.display = '';
+  searchSection.classList.remove("col-12");
+  searchSection.classList.add("col-4");
+  searchedCitiesContainer.style.display = "";
 }
 
 // Save searched cities in localStorage
